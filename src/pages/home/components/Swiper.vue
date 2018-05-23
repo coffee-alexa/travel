2336619ecd4dfd6de7ad298733a4090f1f4e2971
alexa <you19974@qq.com>
2018-05-23 19:09:51 +0800

    lesson 3

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="swiper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" >
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data () {
+        return {
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList: [{
+                id: '0001',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg'
+            },
+            {
+                id: '0002',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/e5/59fad13a64807d02.jpg_750x200_713ae984.jpg'
+            }]
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+    .swiper >>>.swiper-pagination-bullet-active {
+        background: #fff
+    }
+    .swiper {
+        overflow hidden
+        width: 100%
+        height: 0px
+        padding-bottom: 31.2%
+        .swiper-img {
+            width: 100%
+        }
+
+    }
+</style>
